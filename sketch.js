83ff0ff3c76a31ef194747d4ec5cc965f6c76b61
gamerjeannedielman <junes.dreyfus@gmail.com>
2024-11-19 15:27:02 -0500

    Entering
    *Added 3 different entering animations depending on which door is the focus
    *Added eyemovement where screen lightly moves with the mouse
    *Fixed bug where you could enter 2 doors at the same time

--- sketch.js
+++ sketch.js
@@ -2,6 +2,7 @@
 //Which one will you choose/
 //Potentially exploring different playthroughs
 
+
 let state = 'entrance';
 //the first door./
 //this one has a window so you can see what's in it/
@@ -43,39 +44,45 @@ function setup() {
 }
 
 function draw() {
-  frameRate(5);
+  //frameRate(5);
   background('rgb(131,121,110)');
   noStroke();
   
+  //The view slightly adjusts to where the player is looking/
+  //and it's soo subtle/
+  let eyeMovementX = map(mouseX, 0, width, 0, 20);
+  let eyeMovementY = map(mouseY, 0, height, 0, 10);
+  
   push();
   fill(door1.fill);
-  rect (door1.x, door1.y, door1.width, door1.height);
+  rect (door1.x+eyeMovementX, door1.y+eyeMovementY, door1.width, door1.height);
   fill(door1.floor);
-  rect(door1.x, door1.y+door1.height/2, door1.width, door1.height/2);
+  rect(door1.x+eyeMovementX, door1.y+eyeMovementY+door1.height/2, door1.width, door1.height/2);
   pop();
   
   push();
   fill(door2.fill);
-  rect (door2.x, door2.y, door2.width, door2.height);
+  rect (door2.x+eyeMovementX, door2.y+eyeMovementY, door2.width, door2.height);
   fill(door2.floor);
-  rect(door2.x, door2.y+door2.height/2, door2.width, door2.height/2);
+  rect(door2.x+eyeMovementX, door2.y+eyeMovementY+door2.height/2, door2.width, door2.height/2);
   pop();
   
   push();
   fill(door3.fill);
-  rect (door3.x, door3.y, door3.width, door3.height);
+  rect (door3.x+eyeMovementX, door3.y+eyeMovementY, door3.width, door3.height);
   fill(door3.floor);
-  rect(door3.x, door3.y+door3.height/2, door3.width, door3.height/2);
+  rect(door3.x+eyeMovementX, door3.y+eyeMovementY+door3.height/2, door3.width, door3.height/2);
   pop();
 
 }
 
 function mouseWheel(){
   //If mouse is scrolled while hovering on top of the left door you will be entering that one./
-  if(mouseX > door1.x && mouseX < door1.x+door1.width && event.delta > 0 && state === 'entrance' || event.delta > 0 && state === 'entering1'){
-    //The state is now entering 1, a transitional state/
+  if((mouseX > door1.x && mouseX < door1.x+door1.width && event.delta > 0 && state === 'entrance' )||( event.delta > 0 && state === 'entering1')){
+    //The state is now set to entering1, a transitional state/
     //no turning back now/
-  let state = 'entering1';
+  state = 'entering1';
+    
   door1.y-=3;
   door1.x-=0.9;
   door1.width+=15;
@@ -85,12 +92,43 @@ function mouseWheel(){
   door3.x+=30*1.3;
   door2.width+=15;
   door2.height+=15;
-    //
-    
-    
-    
+  
+    //    
   }
      
+    if((mouseX > door2.x && mouseX < door2.x+door2.width && event.delta > 0 && state === 'entrance' )||( event.delta > 0 && state === 'entering2')){
+    //The state is now set to entering2, a transitional state/
+    //no turning back now/
+  state = 'entering2';
+  door2.y-=3;
+  door2.x-=7;
+  door2.width+=15;
+  door2.height+=15;
+  door1.x-=15;
+  door1.y-=3;
+  door1.height+=15;
+  door3.x+=15;
+  door3.y-=3;
+  door3.height+=15;
+    //    
+  }
+  
+      if((mouseX > door3.x && mouseX < door3.x+door3.width && event.delta > 0 && state === 'entrance') || (event.delta > 0 && state === 'entering3')){
+    //The state is now set to entering3, a transitional state/
+    //no turning back now/
+  state = 'entering3';
+  door3.y-=3;
+  door3.x-=15/1.1;
+  door3.width+=15;
+  door3.height+=15;
+  door1.x-=25;
+  door1.y-=3;
+  door1.height+=15;
+  door2.x-=25;
+  door2.y-=3;
+  door2.height+=15;
+  door2.width+=10;
+    //    
+  }
 
 }
-

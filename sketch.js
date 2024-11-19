9ed5bc23d0ef107cef16cd922aa6bc9ecc73acd7
gamerjeannedielman <junes.dreyfus@gmail.com>
2024-11-19 14:34:58 -0500

    Initial commit

---
+++ sketch.js
@@ -0,0 +1,96 @@
+//You are standing in the entrance. Three doors lead to the same room, with small variations. You don't question how they can coexist at the same time and space/
+//Which one will you choose/
+
+let state = 'entrance';
+//the first door./
+//this one has a window so you can see what's in it/
+//If you enter it you will get visual feedback/
+let door1 = {
+  x: 10,
+  y: 10,
+  width: 150,
+  height: 350,
+  fill: 'rgb(183,195,183)',
+  floor: 'rgb(85,71,61)',
+}
+let door2 = {
+  //The second door./
+  //This one has very little light, you will have to touch (click) on the different objects to get a feel for what they are./
+  //If you enter you will get textual feedback/
+  x: 200,
+  y: 10,
+  width: 150,
+  height: 350,
+  fill: 'rgb(48,51,48)',
+  floor: '#64624F',
+}
+let door3 = {
+  //The third door./
+  //This time there is no light at all./
+  //Audio feedback ?/
+  //Maybe more linear/
+  x: 390,
+  y: 10,
+  width: 150,
+  height: 350,
+  fill: 'rgb(48,51,48)',
+  floor: '#2B2A30',
+}
+function setup() {
+  createCanvas(600, 400);
+}
+
+function draw() {
+  frameRate(5);
+  background('rgb(131,121,110)');
+  noStroke();
+  
+  push();
+  fill(door1.fill);
+  rect (door1.x, door1.y, door1.width, door1.height);
+  fill(door1.floor);
+  rect(door1.x, door1.y+door1.height/2, door1.width, door1.height/2);
+  pop();
+  
+  push();
+  fill(door2.fill);
+  rect (door2.x, door2.y, door2.width, door2.height);
+  fill(door2.floor);
+  rect(door2.x, door2.y+door2.height/2, door2.width, door2.height/2);
+  pop();
+  
+  push();
+  fill(door3.fill);
+  rect (door3.x, door3.y, door3.width, door3.height);
+  fill(door3.floor);
+  rect(door3.x, door3.y+door3.height/2, door3.width, door3.height/2);
+  pop();
+
+}
+
+function mouseWheel(){
+  //If mouse is scrolled while hovering on top of the left door you will be entering that one./
+  if(mouseX > door1.x && mouseX < door1.x+door1.width && event.delta > 0 && state === 'entrance' || event.delta > 0 && state === 'entering1'){
+    //The state is now entering 1, a transitional state/
+    //no turning back now/
+  let state = 'entering1';
+  door1.y-=3;
+  door1.x-=0.9;
+  door1.width+=15;
+  door1.height+=15;
+  door2.y-=3;
+  door2.x+= 15*1.3;
+  door3.x+=30*1.3;
+  door2.width+=15;
+  door2.height+=15;
+    //
+    
+    
+    
+  }
+     
+
+}
+
+
+
